f56071e6bce4dd2a956f98f4648a603978ee7e56
nulledusername <lucho.jobly@gmail.com>
2025-04-08 18:22:05 -0300

    refactor: replace React Native Firebase imports with Firebase SDK initialization

--- firebase-js/setup.ts
+++ firebase-js/setup.ts
@@ -1,12 +1,13 @@
+// firebase-js/setup.ts
+import { initializeApp } from 'firebase/app';
+import { getAuth } from 'firebase/auth';
+import { getFirestore } from 'firebase/firestore';
+import { getStorage } from 'firebase/storage';
 import { FIREBASE_CONFIG } from '@/constants/Configurations';
-import firebase from '@react-native-firebase/app';
-import AsyncStorage from '@react-native-async-storage/async-storage';
-import firestore, { FirebaseFirestoreTypes } from '@react-native-firebase/firestore';
-import auth, { FirebaseAuthTypes } from '@react-native-firebase/auth';
-import storage from '@react-native-firebase/storage';
 
-firebase.setReactNativeAsyncStorage(AsyncStorage);
+const app = initializeApp(FIREBASE_CONFIG);
+const auth = getAuth(app);
+const firestore = getFirestore(app);
+const storage = getStorage(app);
 
-const app = firebase;
-
-export { app, auth, FirebaseAuthTypes, FirebaseFirestoreTypes, storage, firestore }
+export { app, auth, firestore, storage };
